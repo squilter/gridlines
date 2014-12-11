d20b94d3868917f5ff80de39482d881a7e6642cb
Samuel Udotong <sudotong@gmail.com>
2014-12-10 20:56:44 -0500

    adding ros velocity and mavlink code

--- getVelocity2.py
+++ getVelocity2.py
@@ -13,33 +13,15 @@ from pymavlink import mavutil
 
 
 #ROOT = '/home/sam/Desktop/pixhawk/src/comm'
-try:
-	ROOT = os.path.dirname(os.path.abspath(__file__))
-except:
-	ROOT = '/home/sam/Desktop/pixhawk/src/comm'
-
-date = str(time.time()) #so we make a unique log each run
-date = date[:-3] #substringing the last two decimal places and decimal off
-
+#try:
+#	ROOT = os.path.dirname(os.path.abspath(__file__))
+#except:
+#	ROOT = '/home/sam/Desktop/pixhawk/src/comm'
 #os.system(ROOT+'/runMavproxy.sh '+date+' '+ROOT) #passing in the unique label as input, and the root
 #myFile = ROOT+'/logs/quadlog'+date+'_decode.txt'
-
-api = local_connect()
-vehicles = api.get_vehicles()
-v = vehicles[0]
-
-print v.location
-print v.attitude
-print v.airspeed
-print v.groundspeed
-
-
-sys.exit()
-
 #print "Preparing to parse "+myFile
 #with open(myFile) as f:
 #    content = f.read().splitlines()
-
 #for line in content:
 #    if "LAND_SPEED" in line:
         #print line
@@ -52,22 +34,42 @@ sys.exit()
 #	heading = 10	
 #	heading = line[20:25]
 #	altSpeed = line[30:40]
+#date = str(time.time()) #so we make a unique log each run
+#date = date[:-3] #substringing the last two decimal places and decimal off
 
-optFlowFrequency = 20
+
 opticalFlowPub = rospy.Publisher('of_meas_post', OpticalFlowPost)
-rospy.init_node('simulator', anonymous=True)
-rate_velocity = rospy.Rate(optFlowFrequency)
+
+api = local_connect()
+vehicles = api.get_vehicles()
+v = vehicles[0]
+
+print v.location
+print v.attitude
+print v.airspeed
+print v.groundspeed
+
+while not api.exit:
+	sleep(10) #milliseconds
+	vx_vy_vz = v.velocity
+	attitude = v.attitude
+	quality = 155 #[0 255]
+	omsg = OpticalFlowPost()
+	omsg.x_vel = vx_vy_vz[0]
+	omsg.y_vel = vx_vy_vz[1]
+	#omsg.z_vel = vx_vy_vz[2]
+	omsg.quality = quality
+	omsg.uncertainty = 1 - (quality/255)
+	rospy.loginfo(omsg)
+	opticalFlowPub.publish(omsg)
+
+sys.exit()
 
 
-omsg = OpticalFlowPost()
-omsg.x_vel=velocity*Math.cos(heading)
-omsg.y_vel=velocity*Math.sin(heading)
-omsg.z_vel=altSpeed
 
-#rospy.loginfo(omsg)
-#optPub.publish(omsg)
-
-
+#optFlowFrequency = 20
+#rospy.init_node('simulator', anonymous=True)
+#rate_velocity = rospy.Rate(optFlowFrequency)
 
 #this loop executes after leaving the above loop and is for cleanup on exit
 #for (m,pm) in mpstate.modules:
---
+++ rosVelocity.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+import mavproxy
+from subprocess import Popen
+import os
+import time
+import rospy
+import sys
+from uav_msgs.msg import OpticalFlowPost
+#from uav_msgs.msg import GridLinePosPost
+from droneapi.lib import *
+from pymavlink import mavutil
+
+
+#ROOT = '/home/sam/Desktop/pixhawk/src/comm'
+#try:
+#	ROOT = os.path.dirname(os.path.abspath(__file__))
+#except:
+#	ROOT = '/home/sam/Desktop/pixhawk/src/comm'
+#os.system(ROOT+'/runMavproxy.sh '+date+' '+ROOT) #passing in the unique label as input, and the root
+#myFile = ROOT+'/logs/quadlog'+date+'_decode.txt'
+#print "Preparing to parse "+myFile
+#with open(myFile) as f:
+#    content = f.read().splitlines()
+#for line in content:
+#    if "LAND_SPEED" in line:
+        #print line
+#	myDate = line[:]
+#	param_values = line.split('{')[1]
+#	params = param_values.split(',')
+#	value = params[1]
+#	velocity = value.split(':')[1]
+#	print "velocity: "+str(velocity)
+#	heading = 10	
+#	heading = line[20:25]
+#	altSpeed = line[30:40]
+#date = str(time.time()) #so we make a unique log each run
+#date = date[:-3] #substringing the last two decimal places and decimal off
+
+velocityFrequency = 20
+
+def getVelocity():
+	print 'Initializing getVelocity....'
+	api = local_connect()
+	vehicles = api.get_vehicles()
+	v = vehicles[0]
+
+	print 'location: '+v.location
+	print 'attitude: '+v.attitude
+	print 'airspeed: '+v.airspeed
+	print 'groundspeed: '+v.groundspeed
+
+	rate1=rospy.Rate(velocityFrequency)
+
+	rate2 = float(rospy.get_param('~rate','1.0'))
+	topic = rospy.get_param('~topic','of_meas_post')
+	rospy.loginfo('rate = %d',rate2)
+	rospy.loginfo('topic = %s',topic)
+	opticalFlowPub = rospy.Publisher('of_meas_post', OpticalFlowPost)
+
+	while not api.exit:
+		vx_vy_vz = v.velocity
+		attitude = v.attitude
+		#quality = 155 #has to be in range [0 255]
+		
+		omsg = OpticalFlowPost()
+		omsg.x_vel = vx_vy_vz[0]
+		omsg.y_vel = vx_vy_vz[1]
+		#omsg.z_vel = vx_vy_vz[2]
+		#omsg.quality = quality
+		#omsg.uncertainty = 1 - (quality/255)
+		opticalFlowPub.publish(omsg)
+
+		rate1.sleep()
+
+	sys.exit()
+
+
+
+	#optFlowFrequency = 20
+	#rospy.init_node('simulator', anonymous=True)
+	rate_velocity = rospy.Rate(optFlowFrequency)
+
+	#this loop executes after leaving the above loop and is for cleanup on exit
+	#for (m,pm) in mpstate.modules:
+	#if hasattr(m, 'unload'):
+	#    print("Unloading module %s" % m.name)
+	#    m.unload()
+
+	#sys.exit(1)
+
+def setPosition(x,y,z,yaw="none"):
+	#https://github.com/mavlink/mavlink/blob/master/message_definitions/v1.0/pixhawk.xml
+
+	#construct a message to set a new offset from the currently controlled position
+	if yaw=="none":
+		msg = v.message_factory.set_position_control_offset_encode(x,y,z)
+	else:
+		msg = v.message_factory.set_position_control_offset_encode(x,y,z,yaw)
+	v.send_mavlink(msg)
+	#Bitmask to indicate which dimensions should be ignored by the vehicle: 
+	#a value of 0b0000000000000000 or 0b0000001000000000 indicates that none of the setpoint dimensions should be ignored. 
+	#If bit 10 is set the floats afx afy afz should be interpreted as force instead of acceleration. 
+	#Mapping: bit 1: x, bit 2: y, bit 3: z, bit 4: vx, bit 5: vy, bit 6: vz, 
+	#bit 7: ax, bit 8: ay, bit 9: az, bit 10: is force setpoint, bit 11: yaw, bit 12: yaw rate
+	msg2 = v.message_factory.position_target_local_ned_encode(x,y,z,vx,vy,vz,afx,afy,afz,yaw,yaw_rate)
+	response2 = v.send_mavlink(msg2)
+
+def setAttitude(roll,pitch,yaw):
+	msg = v.message_factory.attitude_control_encode(roll,pitch,yaw)
+	response = v.send_mavlink(msg)
+	msg2 = v.message_factory.attitude_target_encode(roll,pitch,yaw)
+	response2 = v.send_mavlink(msg2)
+
+def getParameters():
+	return v.parameters
+	
+def getIMU():
+	msg = v.message_factory.scaled_imu_encode() 
+	#timestamp (millisec since system boot)
+	# xacc (mg), yacc (mg), zacc (mg)
+	# w_x (mrad/sec), w_y, w_z
+	# magneticfield_x, B_y, B_z (milli tesla)
+	response = v.send_mavlink(msg)	
+
+def getLocalPos():
+	msg = v.message_factory.local_position_ned_encode()
+	# filtered local position based off accelerometers.
+	#right handed coord frame, z-axis down. north-east-down convention
+	# x y z vx vy vz
+	msg2 = v.message_factory.local_position_ned_cov_encode()
+	# plus covariance	
+	response = v.send_mavlink(msg)	
+	response2 = v.send_mavlink(msg2)
+
+def setOrigin(mylat,mylong,myalt):
+	msg = v.message_factory.set_gps_global_origin_encode(mylat,mylong,myalt)
+	#sets the global mission (0,0,0) point. mavlink supports indoor navigation
+	response = v.send_mavlink(msg)
+
+def getOpticalFlow():
+	msg = v.message_factory.optical_flow_rad_encode()
+	#integration time, integrated_x, integrated)y, integrated_xgyro, ygyro, zgryo,temp, quality, time_delta_distance,distance to center of flow field
+	response = v.send_mavlink(msg)
+
+def injectGPS():
+	raw_data = [110] #i think max length is 110? enough for 12 satellites
+	msg = v.message_factory.gps_inject_data_encode(raw_data.length,raw_data)
+	response = v.send_mavlink(msg)
+
+if __name__=='__main__':
+	try:
+		getVelocity()
+	except rospy.ROSInterruptException: 
+		pass
+
+
