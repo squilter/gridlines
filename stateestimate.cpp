319d84a5a5f0444226ab26cd8e99cd1e0f776393
Samuel Udotong <sudotong@gmail.com>
2015-02-22 15:08:24 -0500

    fixing some stuff on the stateestimator cpp

--- stateestimate.cpp
+++ stateestimate.cpp
@@ -1,6 +1,7 @@
 #include <uORB/uORB.h>
 #include <uORB/topics/estimator_status.h>
 #include <mavlink/mavlink_log.h>
+//#include <string> not sure
 
 static int mavlink_fd;
 mavlink_fd = open(MAVLINK_LOG_DEVICE, 0);
@@ -15,15 +16,17 @@ int init()
 void check_topic()
 {
 	bool updated;
-	struct estimator_status_data ed;
+	struct estimator_status_report esr; // estimator_status_data
  
 	/* check to see whether the topic has updated since the last time we read it */
 	orb_check(topic_handle, &updated);
  
 	if (updated) {
 		/* make a local copy of the updated data structure */
-		orb_copy(ORB_ID(estimator_status), topic_handle, &ed);
-		mavlink_log_info(mavlink_fd, "ESD: "+ed.x+", "+ed.y+", "+ed.z+", "+ed.thrust);
+		orb_copy(ORB_ID(estimator_status), topic_handle, &esr);
+
+//		std::string hostname;	not sure
+		mavlink_log_info(mavlink_fd, "ESR("+esr.n_states+"): "+esr.timestamp+", "+esr.states[0]+", "+esr.states[1]+", "+esr.states[2]+", "+esr.states[3]);
 	}
 }
 
