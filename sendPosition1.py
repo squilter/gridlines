2b082e66b7f42ff5c3b702040e7e1a361800db1e
Samuel Udotong <sudotong@gmail.com>
2015-03-01 12:50:38 -0500

    adding testattitude function and some formatting

--- sendPosition1.py
+++ sendPosition1.py
@@ -20,11 +20,9 @@ class fakeLoc():
 		self.z = z
 
 class UAV():
-
 	def __init__(self):
 		self.api = local_connect()
-
-
+		self.v = None
 	def initialize(self):
 	 	#api = local_connect()
 		self.v = self.api.get_vehicles()[0]
@@ -32,41 +30,36 @@ class UAV():
 		    print "Vehicle still booting, try again later"
 		    return
 		#rospy.Subscriber('way_point_post',WayPointPost,next_cmd)
-
-
-
 	def next_cmd(self, a):#format of these coordinates with respect to gps layout not certain, are we assuming drone has established 	 interface so apiconnect is needed?
-
 		#if not a instanceof WayPointPost:
 		#	print('bad parameter')
 		#	return
-
 		# Use the python gps package to access the laptop GPS
 		# gpsd = gps.gps(mode=gps.WATCH_ENABLE)
 		cmds = self.v.commands
 		#while not self.api.exit:
-		 
-	
 		   # if is_guided and v.mode.name != "GUIDED":
 		       # print "User has changed flight modes - aborting follow-me"
 		       # break
-
 	 	    #if (gpsd.valid & gps.LATLON_SET) != 0:
-		        
 	 	dest = droneapi.lib.Location(a.x,a.y,a.z, is_relative=True)
 	    print "Going to: %s" % dest
 		#cmds.add(dest)
         cmds.goto(dest)
         is_guided = True
         self.v.flush()
+        # Send a new target every ___ seconds
+ #        time.sleep(.1)
 
-	        # Send a new target every ___ seconds
-	        
-        time.sleep(.1)
+	def testAttitude(self):
+		if (self.v != None):
+			while(True):
+				time.sleep(.1)
+				print self.v.attitude.yaw;
 
 uav=UAV()
 uav.initialize()
+uav.testAttitude()
 
 
-
-uav.next_cmd(fakeLoc(100,50,10))
+# uav.next_cmd(fakeLoc(100,50,10))
