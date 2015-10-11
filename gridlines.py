8876bdbcc11e66943be1961f1130e2d4551b35b5
squilter <s.quilter@aol.com>
2015-10-11 13:26:38 -0400

    dd dronekit integration

--- gridlines.py
+++ gridlines.py
@@ -1,5 +1,7 @@
+import time
 import _tkinter
 from Tkinter import *
+from dronekit import connect, mavutil
 
 gridwidth = 1000
 gridheight = 1000
@@ -7,33 +9,64 @@ widthofline = 2.5
 circleRadius = 4
 
 class UAV:
-	
-	def __init__(self, grid):
+	def __init__(self):
+		self.vehicle = connect('udpin:0.0.0.0:14550', await_params=False)
+		self.vehicle.add_attribute_observer('local_position', self.location_callback)
 		self.x = 0
 		self.y = 0
 
-	def goTo(self,x,y):
-		self.setPosition(x*gridwidth/20, y*gridheight/20)
-		self.draw()
+	def set_position(self, x, y):
+		self.x = x*gridwidth/20
+		self.y = y*gridheight/20
+		
+	def location_callback(self, location):
+		print self.vehicle.local_position
+		self.set_position(self.vehicle.local_position.north, self.vehicle.local_position.east)
+	
+	def start_vehicle(altitude):
+		if v.mode.name == "INITIALISING":
+			time.sleep(1)
+		while v.gps_0.fix_type < 2:
+			time.sleep(1)
 
-	def draw(self):
-		self.grid.drawUAV(self.x,self.y)
-
-	def updatePosition(self, x, y):
-		self.x = x
-		self.y = y
-
+		v.armed = True
+		v.flush()
+	
+		while not v.armed:
+			time.sleep(1)
+	
+		vehicle.mode = VehicleMode("GUIDED")
+		Vehicle.commands.takeoff(altitude)
+		
+	def goto_local_position(self, north, east, down):
+		if(self.vehicle.armed is False):
+			self.start_vehicle(5)
+	
+		msg = self.vehicle.message_factory.set_position_target_local_ned_encode(
+			0,       # time_boot_ms (not used)
+			0, 0,    # target system, target component
+			mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+			0b0000111111111000, # type_mask (only positions enabled)
+			north, east, down, # x, y, z positions (or North, East, Down in the MAV_FRAME_BODY_NED frame
+			0, 0, 0, # x, y, z velocity in m/s  (not used)
+			0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+			0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
+		self.vehicle.send_mavlink(msg)
+		self.vehicle.flush()	
+	
 
 class Grid:
 
-	def __init__(self):
+	def __init__(self, uav):
+		self.uav = uav
+		
 		root = Tk()
 		#app = Application(master=root)
 		self.canvas = Canvas(root, width=gridwidth, height=gridheight)
 		self.canvas.bind('<Button-1>', self.onCanvasClick) 
 		self.canvas.pack()
 		self.canvas.create_rectangle(0,0,gridheight,gridwidth,fill="gray")
-		self.drawGrid(self.canvas)
+		self.drawGrid(self.canvas, uav)
 		root.mainloop()
 
 	def drawLine(self, canvas, x0, y0, x1, y1):
@@ -64,6 +97,7 @@ class Grid:
 
 	def onCanvasClick(self, event):
 		print 'Got canvas click', event.x, event.y
+		self.uav.goto_local_position(event.x/50, event.y/50, -5)
 
 	def drawUAV(self, uav):
 		#update uav
@@ -74,15 +108,9 @@ class Grid:
 
 
 uav=UAV()
-g=Grid()
+g=Grid(uav)
 while True:
 	g.clearCanvas()
 	g.drawGrid(uav)
+	time.sleep(0.1)
 	#g.updateUAV()
-
-
-
-
-
-
-
